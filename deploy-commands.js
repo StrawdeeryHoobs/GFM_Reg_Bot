d003ec64d94e2ae40526e732e177968439c3d776
StrawdeeryHoobs <hoobs@gatewayfurmeet.org>
2021-11-30 14:46:53 -0600

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,7 +1,18 @@
 const fsLibrary = require('fs');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { clientId, guildId, token } = require('./config.json');
+//const { clientId, guildId } = require('./config.json');
+
+let guildId = '915295538373599304';
+let clientId = '915077946648182804'
+try{
+    
+    DiscordToken = fsLibrary.readFileSync('DiscordToken.txt', 'utf8');
+
+}catch(error){
+
+    console.log("No file exists for 'DiscordToken.txt'");
+}
 
 const commands = [];
 const commandFiles = fsLibrary.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -11,7 +22,7 @@ for (const file of commandFiles) {
 	commands.push(command.data.toJSON());
 }
 
-const rest = new REST({ version: '9' }).setToken(token);
+const rest = new REST({ version: '9' }).setToken(DiscordToken);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
