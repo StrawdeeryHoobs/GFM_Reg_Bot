7e0703c8e21547fe48dcac32d3feb1c55d54df1c
StrawdeeryHoobs <hoobs@gatewayfurmeet.org>
2021-12-04 14:23:47 -0600

    Update index.js

--- index.js
+++ index.js
@@ -4,14 +4,6 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.commands = new Collection();
-const commandFiles = fsLibrary.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.data.name, command);
-}
-
 client.once('ready', () => {
 	console.log('Ready!');
 });
